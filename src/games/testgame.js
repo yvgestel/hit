7dc90845a9b206b3588ef2a159ef49e25959aa9e
Youp van Gestel <yvgestel@outlook.com>
2023-01-10 13:39:55 +0100

    different try mobile screen size

--- src/games/testgame.js
+++ src/games/testgame.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef } from 'react';
 import useWindowResize from '../hooks/useWindowResize'
-import Webcam from "react-webcam";
 import * as THREE from 'three';
 import './testgame.css';
 
@@ -8,49 +7,65 @@ export const TestGame = () => {
     const { screenWidth, screenHeight } = useWindowResize()
     const videoRef = useRef(null)
 
-    // const getVideo = (screenHeight, screenWidth) => {
-    //     navigator.mediaDevices.getUserMedia({
-    //         video: {width: screenWidth, height: screenHeight}
-    //     }).then(stream => {
-    //         let video = videoRef.current
-    //         video.srcObject = stream
-    //         video.play()
-    //     }).catch(err => {
-    //         console.error(err)
-    //     })
-    // }
+    const getVideo = (screenHeight, screenWidth) => {
+        let video = videoRef.current
+        video.setAttribute('width', screenWidth);
+        video.setAttribute('height', screenHeight);
+        navigator.mediaDevices.getUserMedia({
+            video: {width: screenWidth, height: screenHeight}
+        }).then(stream => {
+            video.play()
+        }).catch(err => {
+            console.error(err)
+        })
+    }
 
-    // useEffect(() => {
-    //     getVideo(screenHeight, screenWidth)
-    // }, [videoRef,screenHeight, screenWidth])
+    useEffect(() => {
+        getVideo(screenHeight, screenWidth)
+    }, [videoRef,screenHeight, screenWidth])
 
     return (
         <div className='game-page-container'>
+            <video
+                id='game-container'
+                className='game-container'
+                ref={videoRef}
+            /> 
             <div 
-                style={{
-                    width: screenWidth,
-                    height: screenHeight,
-                }}
-            >
-                <p>{screenHeight}+' '+{screenWidth}</p>
-            </div>
+                id='game-container'
+                className='game-container'
+            /> 
         </div>
 
     )
 }
 
-// {/* <Webcam
-// id='video-container'
-// className='video-container' 
-// audio={false}
-// ref={videoRef}
-// videoConstraints={{
-//     width: screenWidth,
-//     height: screenHeight,
-//     facingMode: "user"
-//   }}
-// />
-// <div 
-// id='game-container'
-// className='game-container'
-// /> */}
+
+
+
+//   var video = $('body > video')[0];
+  
+//   navigator.getMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+  
+//   navigator.getMedia({ video: !0, audio: !1 }, function(stream)
+//   {
+//     if ('srcObject' in video) {
+//         video.srcObject = stream;
+//       } else {
+//         video.src = vu.createObjectURL(stream);
+//       }
+//     video.play();
+//   }, function(error)
+
+
+  
+//   video.addEventListener('canplay', function(ev)
+//   {
+//     if(!streaming)
+//     {
+//       height = video.videoHeight / (video.videoWidth / width);
+//       video.setAttribute('width', width);
+//       video.setAttribute('height', height);
+//       streaming = !0;
+//     }
+//   }, !1);
