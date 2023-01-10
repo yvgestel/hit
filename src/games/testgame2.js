8c889c20c31f22c30ac658f0ed287808919938ee
Youp van Gestel <yvgestel@outlook.com>
2023-01-10 16:39:55 +0100

    Trying Camera package without other divs

--- src/games/testgame2.js
+++ src/games/testgame2.js
@@ -33,41 +33,13 @@ export const TestGame = () => {
                 facingMode='user'
                 aspectRatio='cover'
             />
-            <div 
-                id='game-container'
-                className='game-container'
-            /> 
         </div>
 
     )
 }
 
 
-
-
-//   var video = $('body > video')[0];
-  
-//   navigator.getMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
-  
-//   navigator.getMedia({ video: !0, audio: !1 }, function(stream)
-//   {
-//     if ('srcObject' in video) {
-//         video.srcObject = stream;
-//       } else {
-//         video.src = vu.createObjectURL(stream);
-//       }
-//     video.play();
-//   }, function(error)
-
-
-  
-//   video.addEventListener('canplay', function(ev)
-//   {
-//     if(!streaming)
-//     {
-//       height = video.videoHeight / (video.videoWidth / width);
-//       video.setAttribute('width', width);
-//       video.setAttribute('height', height);
-//       streaming = !0;
-//     }
-//   }, !1);
+// <div 
+// id='game-container'
+// className='game-container'
+// />  
