d849ceafb36a97dfc7c64ad61d76777ecb0d1f1b
Youp van Gestel <yvgestel@outlook.com>
2023-01-11 20:37:52 +0100

    removed div

--- src/games/testgame.js
+++ src/games/testgame.js
@@ -30,41 +30,15 @@ export const TestGame = () => {
                 className='video-container'
                 ref={videoRef}
             /> 
-            <div 
-                id='game-container'
-                className='game-container'
-            >
-                <h1>Test</h1>
-            </div>  
         </div>
 
     )
 }
 
 
-//   var video = $('body > video')[0];
-  
-//   navigator.getMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
-  
-//   navigator.getMedia({ video: !0, audio: !1 }, function(stream)
-//   {
-//     if ('srcObject' in video) {
-//         video.srcObject = stream;
-//       } else {
-//         video.src = vu.createObjectURL(stream);
-//       }
-//     video.play();
-//   }, function(error)
-
-
-  
-//   video.addEventListener('canplay', function(ev)
-//   {
-//     if(!streaming)
-//     {
-//       height = video.videoHeight / (video.videoWidth / width);
-//       video.setAttribute('width', width);
-//       video.setAttribute('height', height);
-//       streaming = !0;
-//     }
-//   }, !1);
+// {/* <div 
+// id='game-container'
+// className='game-container'
+// >
+// <h1>Test</h1>
+// </div>   */}
