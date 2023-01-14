6c6f7510688d33aeaa91c7de96e963cf7c3fde5e
Youp van Gestel <yvgestel@outlook.com>
2023-01-14 10:43:31 +0100

    trying first AR component

--- src/games/testgame.js
+++ src/games/testgame.js
@@ -6,6 +6,7 @@ import './testgame.css';
 export const TestGame = () => {
     const { screenWidth, screenHeight } = useWindowResize()
     const videoRef = useRef(null)
+    const canvasRef = useRef(null)
 
     const getVideo = (screenHeight, screenWidth) => {
         let video = videoRef.current
@@ -20,17 +21,33 @@ export const TestGame = () => {
     }
 
     useEffect(() => {
+        const scene = new THREE.Scene()
+        const geometry = new THREE.BoxGeometry(1, 1, 1)
+        const material = new THREE.MeshBasicMaterial({color: '#0000FF'})
+        const cube = new THREE.Mesh(geometry, material)
+
+        scene.add(cube)
+        cube.position.set(0, 0, -2)
+        cube.rotation.set(0, Math.PI/4, 0)
+
+        const camera = new THREE.PerspectiveCamera()
+        camera.position.set(1, 1, 5)
+
+        const renderer = canvasRef.current
+        renderer.render(scene, camera)
+    },[])
+
+    useEffect(() => {
         getVideo(screenHeight, screenWidth)
     }, [videoRef,screenHeight, screenWidth])
 
     return (
         <div className='game-page-container'>
-            <div 
+            <canvas 
                 id='game-container'
                 className='game-container'
-            >
-                <h1 className='text-zwart'>Test</h1>
-            </div>   
+                ref={canvasRef}
+            />  
             <video
                 id='video-container'
                 className='video-container'
--- src/games/testgame2.js
+++ src/games/testgame2.js
@@ -9,10 +9,10 @@ export const TestGame = () => {
     const videoRef = useRef(null)
 
     const getVideo = (screenHeight, screenWidth) => {
-        let video = videoRef.current
         navigator.mediaDevices.getUserMedia({
             video: {width: screenWidth, height: screenHeight}
         }).then(stream => {
+            let video = videoRef.current
             video.srcObject = stream
             video.play()
         }).catch(err => {
@@ -21,17 +21,35 @@ export const TestGame = () => {
     }
 
     useEffect(() => {
-        getVideo(screenHeight, screenWidth)
-    }, [videoRef,screenHeight, screenWidth])
+        const scene = new THREE.Scene()
+        const geometry = new THREE.BoxGeometry(1, 1, 1)
+        const material = new THREE.MeshBasicMaterial({color: '#0000FF'})
+        const cube = new THREE.Mesh(geometry, material)
+
+        scene.add(cube)
+        cube.position.set(0, 0, -2)
+        cube.rotation.set(0, Math.PI/4, 0)
+
+        const camera = new THREE.PerspectiveCamera()
+        camera.position.set(1, 1, 5)
+
+        const renderer = new THREE.WebGLRenderer({
+            alpha: true,
+            canvas: document.getElementById('game-container'),
+        })
+        renderer.render(scene, camera)
+    },[])
+
+    // useEffect(() => {
+    //     getVideo(screenHeight, screenWidth)
+    // }, [videoRef,screenHeight, screenWidth])
 
     return (
         <div className='game-page-container'>
-            <div 
+            <canvas 
                 id='game-container'
                 className='game-container'
-            >
-                <h1>Test</h1>
-            </div>  
+            />
             <Camera 
                 id='video-container'
                 className='video-container'
